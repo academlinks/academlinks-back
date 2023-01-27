c3dd6a10ea8add14aa33448bd8680d9820b1708f
JohnnyRuss <georgiaspontoeli123@gmail.com>
2023-01-27 10:56:03 +0400

    x

--- src/lib/getAppConnection.js
+++ src/lib/getAppConnection.js
@@ -3,9 +3,10 @@ const DB_DEV_APP_CONNECTION = process.env.DB_DEV_APP_CONNECTION;
 const DB_APP_CONNECTION = process.env.DB_APP_CONNECTION;
 
 function getAppConnection() {
-  if (NODE_MODE === DEV || NODE_MODE === "TEST_PROD")
+  if (NODE_MODE === "DEV" || NODE_MODE === "TEST_PROD")
     return DB_DEV_APP_CONNECTION;
-  else return DB_APP_CONNECTION;
+  else if (NODE_MODE === "PROD") return DB_APP_CONNECTION;
+  else return "";
 }
 
 module.exports = getAppConnection;
