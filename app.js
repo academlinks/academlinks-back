b7d601ce95e5bb1ed78239a33f35f525ebb2f8a4
JohnnyRuss <georgiaspontoeli123@gmail.com>
2023-01-29 11:47:04 +0400

    cc

--- app.js
+++ app.js
@@ -29,28 +29,6 @@ const { getOrigins } = require("./src/lib/getOrigins");
 
 const App = express();
 
-App.use(function (req, res, next) {
-  // const allowedOrigins = getOrigins();
-  // const origin = req.headers.origin;
-
-  // if (allowedOrigins.includes(origin)) {
-  //   res.setHeader("Access-Control-Allow-Origin", origin);
-  // }
-
-  res.header("Access-Control-Allow-credentials", true);
-  res.header("Access-Control-Allow-Origin", getOrigins().join(","));
-  res.header(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, authorization"
-  );
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, UPDATE, OPTIONS"
-  );
-
-  next();
-});
-
 process.env.NODE_MODE === "DEV" && App.use(morgan("dev"));
 
 // const __filename = fileURLToPath(import.meta.url);
@@ -71,11 +49,26 @@ App.use(xss());
 App.use(hpp());
 
 App.use(cookieParser());
+
+App.use(function (req, res, next) {
+  res.header("Access-Control-Allow-credentials", true);
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, authorization"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, UPDATE, OPTIONS"
+  );
+
+  next();
+});
+
 App.use(
   cors({
     credentials: true,
     origin: function (origin, callback) {
-      console.log(getOrigins());
       if (!origin) return callback(null, true);
       if (getOrigins().indexOf(origin) === -1) {
         const msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
