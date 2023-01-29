6c9297ea8c5abd0c94c5e0d8bb1e2cb7e6ff4cf2
JohnnyRuss <georgiaspontoeli123@gmail.com>
2023-01-29 16:30:51 +0400

    ff

--- app.js
+++ app.js
@@ -50,16 +50,13 @@ App.use(cookieParser());
 App.use(function (req, res, next) {
   res.header("Access-Control-Allow-credentials", true);
   res.header("Access-Control-Allow-Origin", req.headers.origin);
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, UPDATE, OPTIONS"
-  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
   res.header(
     "Access-Control-Allow-Headers",
     "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Origin, Authorization"
   );
 
-  if (req.method === "OPTIONS") res.send(200);
+  if (req.method === "OPTIONS") res.sendStatus(200);
   else next();
 });
 App.use(
