8c21d70da4213dcdf67ff243c93c497e78db2cc0
JohnnyRuss <georgiaspontoeli123@gmail.com>
2023-01-29 10:58:22 +0400

    headers c

--- app.js
+++ app.js
@@ -30,18 +30,24 @@ const { getOrigins } = require("./src/lib/getOrigins");
 const App = express();
 
 App.use(function (req, res, next) {
-  // res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "*");
+
   const allowedOrigins = getOrigins();
   const origin = req.headers.origin;
+  
   if (allowedOrigins.includes(origin)) {
     res.setHeader("Access-Control-Allow-Origin", origin);
   }
+  
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
   res.header("Access-Control-Allow-credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, UPDATE, OPTIONS"
+  );
 
   next();
 });
