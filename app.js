ce38f3223bb546a0ec38b4ec5f445d7789909e12
JohnnyRuss <georgiaspontoeli123@gmail.com>
2023-02-12 18:11:18 +0400

    kk

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ function getStaticFileDestination() {
   const staticPath =
     NODE_MODE === "DEV"
       ? path.join(__dirname, "public/images")
-      : path.join(__dirname, "../../../../", "public/images");
+      : path.join(__dirname, "../../../..", "public/images");
 
   console.log({ dir: __dirname, staticPath });
 
